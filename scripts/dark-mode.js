97b23975cdd18a0504f10cce8cf5009bbe9b98dc
nefiserrano <nefi_3902@hotmail.com>
2024-05-15 02:21:18 -0600

    Team Activity Week 2 Finished

---
+++ scripts/dark-mode.js
@@ -0,0 +1,17 @@
+const darkButton = document.querySelector('#darkMode');
+const main = document.querySelector('main');
+const h1 = document.querySelector('h1');
+const firstCard = document.querySelector('#card1');
+const secondCard = document.querySelector('#card2');
+
+darkButton.addEventListener('click', () => {
+    main.classList.toggle('dark');
+    h1.classList.toggle('light');
+    firstCard.classList.toggle('darker');
+    secondCard.classList.toggle('darker');
+    if (main.classList.contains('dark')) {
+        darkButton.src = "images/day-mode.png";
+    } else {
+        darkButton.src = "images/night-mode.png";
+    }
+})
